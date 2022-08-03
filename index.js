daa9a43f03ac21276803ce760b735ad3fe05b93c
gonz4216 <ethgon19@gmail.com>
2022-08-03 00:59:26 -0400

    main

--- index.js
+++ index.js
@@ -1,116 +1,116 @@
-// const add = (numOne, numTwo) => {
-//    return numOne + numTwo
+const add = (numOne, numTwo) => {
+   return numOne + numTwo
  
-// }
-// console.log(add(2, 1))
+}
+console.log(add(2, 1))
 
 // ----------------------------------------------------
 
-// const hoursSecs = (num) => {
-// return num * 3600
-// }
-// console.log(hoursSecs(3))
-
-
-// ----------------------------------------------------
-
-
-// const preRec = (num1, num2) => {
-// return 2 * num1 + 2 * num2
-// }
-
-// console.log(preRec(20, 10))
+const hoursSecs = (num) => {
+return num * 3600
+}
+console.log(hoursSecs(3))
 
 
 // ----------------------------------------------------
 
 
-// let string = ["hello", "bye"]
-// const str = () => {
-//     return string.push("sum")
+const preRec = (num1, num2) => {
+return 2 * num1 + 2 * num2
+}
 
-// }
-
-// str()
-// console.log(string)
+console.log(preRec(20, 10))
 
 
 // ----------------------------------------------------
 
 
-// const name = (num1, num2) => {
-//     if (num1 + num2 >= 100) {
-//         return true
-//     } else{
-//         return false
-//     }
-// }
-// name()
+let string = ["hello", "bye"]
+const str = () => {
+    return string.push("sum")
 
-// console.log(name(99, 1))
+}
 
-
-// ----------------------------------------------------
-
-// const name = (num1) => {
-// return num1 <= 0
-// }
-// console.log(name(0))
+str()
+console.log(string)
 
 
 // ----------------------------------------------------
 
 
-// function boolean(sum) {
-// if(sum === true){
-//     return false
-// }if(sum === false){
-//     return true
-// }
-// }
+const name = (num1, num2) => {
+    if (num1 + num2 >= 100) {
+        return true
+    } else{
+        return false
+    }
+}
+name()
 
-// console.log(boolean(2))
-
-
-// ----------------------------------------------------
-
-// function remainder(num1, num2) {
-// return num1 % num2
-// }
-// console.log(remainder(9, 8))
+console.log(name(99, 1))
 
 
 // ----------------------------------------------------
 
+const name = (num1) => {
+return num1 <= 0
+}
+console.log(name(0))
 
-// function isOdd(num) {
-// return num % 2 === 0
-// }
-
-// console.log(isOdd(2))
 
 // ----------------------------------------------------
 
 
-// function isEven(num) {
+function boolean(sum) {
+if(sum === true){
+    return false
+}if(sum === false){
+    return true
+}
+}
 
-//     if(num % 2 === 0){
-//         return 1
-//     }return -1
+console.log(boolean(2))
 
-// }
 
-// console.log(isEven(1))
+// ----------------------------------------------------
+
+function remainder(num1, num2) {
+return num1 % num2
+}
+console.log(remainder(9, 8))
+
 
 // ----------------------------------------------------
 
 
-// function loggedOut (str, str2) {
-// return str === 'HELLO' && str2 === 'GOODBYE'
+function isOdd(num) {
+return num % 2 === 0
+}
 
-// }
+console.log(isOdd(2))
 
-// console.log(loggedOut('HELLO', 'GOODBYE'))
+// ----------------------------------------------------
+
+
+function isEven(num) {
+
+    if(num % 2 === 0){
+        return 1
+    }return -1
+
+}
+
+console.log(isEven(1))
+
+// ----------------------------------------------------
+
+
+function loggedOut (str, str2) {
+return str === 'HELLO' && str2 === 'GOODBYE'
+
+}
+
+console.log(loggedOut('HELLO', 'GOODBYE'))
 
 
 // ----------------------------------------------------
