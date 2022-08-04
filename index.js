bcbbe7e84ad712947015e74d8788f937f3e711b0
gonz4216 <ethgon19@gmail.com>
2022-08-04 02:20:46 -0400

    main

--- index.js
+++ index.js
@@ -38,24 +38,24 @@ console.log(string)
 // ----------------------------------------------------
 
 
-const name = (num1, num2) => {
+const nameThing = (num1, num2) => {
     if (num1 + num2 >= 100) {
         return true
     } else{
         return false
     }
 }
-name()
+nameThing()
 
-console.log(name(99, 1))
+console.log(nameThing(99, 1))
 
 
 // ----------------------------------------------------
 
-const name = (num1) => {
+const nameName = (num1) => {
 return num1 <= 0
 }
-console.log(name(0))
+console.log(nameName(0))
 
 
 // ----------------------------------------------------
@@ -114,3 +114,174 @@ console.log(loggedOut('HELLO', 'GOODBYE'))
 
 
 // ----------------------------------------------------
+
+
+function flasyOr(num1, num2) {
+    return (!num1) ? num1 : num2
+}
+
+console.log(flasyOr(null,2))
+
+
+// ----------------------------------------------------
+
+function arrLength(arr) {
+    
+ console.log(arr.length)
+    
+return arr
+}
+
+
+arrLength([1,4,5,4,1])
+
+// ----------------------------------------------------
+
+
+function sumOfArray(arr) {
+    let sum = 0
+    for (let i = 0; i < arr.length; ++i){
+        sum += arr[i]
+    }
+    console.log(sum)
+}
+
+sumOfArray([1,3,5])
+
+
+// ----------------------------------------------------
+
+
+function addNum(num) {
+    let sum = 0
+    for (let i = 0; i <= num; ++i){
+        sum += i
+         
+    }
+    console.log(sum)
+    return sum
+   
+}
+
+addNum(3)
+
+
+// ----------------------------------------------------
+
+function calctime(secs) {
+    let arr = [5, 7, 2]
+    let newi = Math.max(1,2,5,4)
+
+    return console.log(newi)
+}
+calctime()
+
+
+// ----------------------------------------------------
+
+
+function reverseString(str) {
+let split = str.split('')
+
+let reverse = split.reverse("")
+
+let join = reverse.join("")
+return console.log(join)
+}
+reverseString("hello");
+
+// ----------------------------------------------------
+
+
+
+function reverseString(str) {
+    for(let i = 0; i < str.length; ++i) {
+        str[i] = 0  
+    }
+    return str
+}
+
+
+console.log(reverseString([2,4,6,7]))
+
+// ----------------------------------------------------
+
+
+function fruits(value) {
+value = value.filter(e => e !== "apple")
+    return value
+
+}
+    console.log(fruits(["banana", "apple", "pear"]))
+
+
+
+    
+    function fruits(value) {
+        let arr = ['A', 'B', 'C'];
+        arr = arr.filter(e => e !== 'B');
+        return console.log(arr)
+    
+    }
+        fruits()
+
+
+    
+// ----------------------------------------------------
+
+function fitlerFalse(arr) {
+
+return arr.filter(e => !!e === true)
+
+}
+console.log(fitlerFalse([5,1,2,0,""]))
+
+
+
+
+function fitlerFalse(arr) {
+    let newArr = []
+for (let i = 0;i < arr.length; ++i){
+    if(!arr[i] === true) {
+       newArr.push(arr[i])
+    }
+   
+}
+return newArr
+}
+console.log(fitlerFalse([5,1,2,0,""]))
+
+
+// ----------------------------------------------------
+
+
+let arr = ["", 1,2,0]
+let newArr = []
+for(let i = 0; i < arr.length; i++){
+  
+   newArr.push(!!arr[i])
+}
+
+console.log(newArr)
+
+
+// ----------------------------------------------------
+
+
+function star(num) {
+    if(num === 1){
+        return "*"
+    }else if(num === 2){
+        return "**"
+    }
+}
+
+console.log(star(2))
+
+
+
+// ----------------------------------------------------
+
+
+
+ 
